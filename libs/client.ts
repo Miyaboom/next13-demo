d4dae030d5022aeb62455e7d4c54c398f41a7022
“Miyaboom” <miyaboo0825@gmail.com>
2023-06-05 11:36:28 +0900

    API エンドポイント修正

--- libs/client.ts
+++ libs/client.ts
@@ -7,7 +7,7 @@ export const { getClient } = registerApolloClient(() => {
     link: new HttpLink({
       // https://studio.apollographql.com/public/spacex-l4uc6p/
       uri: process.env.VERCEL_URL
-        ? `${process.env.VERCEL_URL}/api/graphql`
+        ? `https://${process.env.VERCEL_URL}/api/graphql`
         : 'http://localhost:3002/api/graphql'
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
